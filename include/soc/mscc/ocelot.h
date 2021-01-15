c8f29f75177685af7d2a938f56497f21c301d72c
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-01-15 04:11:18 +0200

    net: mscc: ocelot: register devlink ports
    Add devlink integration into the mscc_ocelot switchdev driver. All
    physical ports (i.e. the unused ones as well) except the CPU port module
    at ocelot->num_phys_ports are registered with devlink, and that requires
    keeping the devlink_port structure outside struct ocelot_port_private,
    since the latter has a 1:1 mapping with a struct net_device (which does
    not exist for unused ports).
    
    Since we use devlink_port_type_eth_set to link the devlink port to the
    net_device, we can as well remove the .ndo_get_phys_port_name and
    .ndo_get_port_parent_id implementations, since devlink takes care of
    retrieving the port name and number automatically, once
    .ndo_get_devlink_port is implemented.
    
    Note that the felix DSA driver is already integrated with devlink by
    default, since that is a thing that the DSA core takes care of. This is
    the reason why these devlink stubs were put in ocelot_net.c and not in
    the common library. It is also the reason why ocelot::devlink is a
    pointer and not a full structure embedded inside struct ocelot: because
    the mscc_ocelot driver allocates that by itself (as the container of
    struct ocelot, in fact), but in the case of felix, it is DSA who
    allocates the devlink, and felix just propagates the pointer towards
    struct ocelot.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 6c30384eb1dec96b678ff9c01c15134b1a0e81f4)
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- include/soc/mscc/ocelot.h
+++ include/soc/mscc/ocelot.h
@@ -625,6 +625,8 @@ struct ocelot_port {
 
 struct ocelot {
 	struct device			*dev;
+	struct devlink			*devlink;
+	struct devlink_port		*devlink_ports;
 
 	const struct ocelot_ops		*ops;
 	struct regmap			*targets[TARGET_MAX];
