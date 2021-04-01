2a65aa0772f9665f1680c3d8bdb462465ed3a072
Ong Boon Leong <boon.leong.ong@intel.com>
2021-04-01 10:11:15 +0800

    net: stmmac: Add initial XDP support
    This patch adds the initial XDP support to stmmac driver. It supports
    XDP_PASS, XDP_DROP and XDP_ABORTED actions. Upcoming patches will add
    support for XDP_TX and XDP_REDIRECT.
    
    To support XDP headroom, this patch adds page_offset into RX buffer and
    change the dma_sync_single_for_device|cpu(). The DMA address used for
    RX operation are changed to take into page_offset too. As page_pool
    can handle dma_sync_single_for_device() on behalf of driver with
    PP_FLAG_DMA_SYNC_DEV flag, we skip doing that in stmmac driver.
    
    Current stmmac driver supports split header support (SPH) in RX but
    the flexibility of splitting header and payload at different position
    makes it very complex to be supported for XDP processing. In addition,
    jumbo frame is not supported in XDP to keep the initial codes simple.
    
    This patch has been tested with the sample app "xdp1" located in
    samples/bpf directory for both SKB and Native (XDP) mode. The burst
    traffic generated using pktgen_sample03_burst_single_flow.sh in
    samples/pktgen directory.
    
    Changes in v3:
     - factor in xdp header and tail adjustment done by XDP program.
       Thanks to Jakub Kicinski for pointing out the gap in v2.
    
    Changes in v2:
     - fix for "warning: variable 'len' set but not used" reported by lkp.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac.h
+++ drivers/net/ethernet/stmicro/stmmac/stmmac.h
@@ -63,8 +63,9 @@ struct stmmac_tx_queue {
 
 struct stmmac_rx_buffer {
 	struct page *page;
-	struct page *sec_page;
 	dma_addr_t addr;
+	__u32 page_offset;
+	struct page *sec_page;
 	dma_addr_t sec_addr;
 };
 
@@ -247,6 +248,9 @@ struct stmmac_priv {
 
 	/* Receive Side Scaling */
 	struct stmmac_rss rss;
+
+	/* XDP BPF Program */
+	struct bpf_prog *xdp_prog;
 };
 
 enum stmmac_state {
@@ -263,6 +267,8 @@ void stmmac_set_ethtool_ops(struct net_device *netdev);
 
 void stmmac_ptp_register(struct stmmac_priv *priv);
 void stmmac_ptp_unregister(struct stmmac_priv *priv);
+int stmmac_open(struct net_device *dev);
+int stmmac_release(struct net_device *dev);
 int stmmac_resume(struct device *dev);
 int stmmac_suspend(struct device *dev);
 int stmmac_dvr_remove(struct device *dev);
@@ -276,6 +282,19 @@ int stmmac_reinit_ringparam(struct net_device *dev, u32 rx_size, u32 tx_size);
 struct timespec64 stmmac_calc_tas_basetime(ktime_t old_base_time,
 					   ktime_t current_time,
 					   u64 cycle_time);
+
+static inline bool stmmac_xdp_is_enabled(struct stmmac_priv *priv)
+{
+	return !!priv->xdp_prog;
+}
+
+static inline unsigned int stmmac_rx_offset(struct stmmac_priv *priv)
+{
+	if (stmmac_xdp_is_enabled(priv))
+		return XDP_PACKET_HEADROOM;
+
+	return 0;
+}
 
 #if IS_ENABLED(CONFIG_STMMAC_SELFTESTS)
 void stmmac_selftest_run(struct net_device *dev,
