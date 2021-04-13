e713cfb3d0d901d00fc03fe4b13a8ea37e715abd
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-04-13 15:09:20 +0800

    net: mscc: ocelot: export MAC table learn and forget
    Felix DSA needs to use these operations as well, for its 802.1CB stream
    ID manipulation functions. So export them in preparation of that.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/ethernet/mscc/ocelot.c
+++ drivers/net/ethernet/mscc/ocelot.c
@@ -12,12 +12,6 @@
 #define TABLE_UPDATE_SLEEP_US 10
 #define TABLE_UPDATE_TIMEOUT_US 100000
 
-struct ocelot_mact_entry {
-	u8 mac[ETH_ALEN];
-	u16 vid;
-	enum macaccess_entry_type type;
-};
-
 static inline u32 ocelot_mact_read_macaccess(struct ocelot *ocelot)
 {
 	return ocelot_read(ocelot, ANA_TABLES_MACACCESS);
@@ -742,8 +736,8 @@ nla_put_failure:
 }
 EXPORT_SYMBOL(ocelot_port_fdb_do_dump);
 
-static int ocelot_mact_read(struct ocelot *ocelot, int row, int col, int *dst,
-			    struct ocelot_mact_entry *entry)
+int ocelot_mact_read(struct ocelot *ocelot, int row, int col, int *dst,
+		     struct ocelot_mact_entry *entry)
 {
 	u32 val, macl, mach;
 	char mac[ETH_ALEN];
@@ -785,6 +779,7 @@ static int ocelot_mact_read(struct ocelot *ocelot, int row, int col, int *dst,
 
 	return 0;
 }
+EXPORT_SYMBOL(ocelot_mact_read);
 
 int ocelot_fdb_dump(struct ocelot *ocelot, int port,
 		    dsa_fdb_dump_cb_t *cb, void *data)
