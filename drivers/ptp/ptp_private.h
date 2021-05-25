c7eee7f6d91b83f77755c7f4379cb58ec6c378a5
Yangbo Lu <yangbo.lu@nxp.com>
2021-05-25 10:30:20 +0800

    ptp: add ptp virtual clock driver framework
    This patch is to add ptp virtual clock driver framework
    which just exports essential APIs.
    
    A new member is added for ptp_clock_info structure. Device driver
    can provide initial cyclecounter info for ptp virtual clock via
    this member, before normally registering ptp clock.
    
    - struct ptp_vclock_cc *vclock_cc;
    
    PTP vclock register/unregister APIs are private for PTP driver.
    They can be called after normal ptp clock registering.
    
    - ptp_vclock_register()
    - ptp_vclock_unregister()
    
    And below API added is for device driver to get ptp_clock_info of
    registered physical clock through cyclecounter pointer of ptp virtual
    clock. This is needed for cyclecounter .read callback to read
    physical clock cycles.
    
    - ptp_get_pclock_info()
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- drivers/ptp/ptp_private.h
+++ drivers/ptp/ptp_private.h
@@ -3,6 +3,7 @@
  * PTP 1588 clock support - private declarations for the core module.
  *
  * Copyright (C) 2010 OMICRON electronics GmbH
+ * Copyright 2021 NXP
  */
 #ifndef _PTP_PRIVATE_H_
 #define _PTP_PRIVATE_H_
@@ -48,6 +49,26 @@ struct ptp_clock {
 	struct kthread_delayed_work aux_work;
 };
 
+#define info_to_vclock(d) container_of((d), struct ptp_vclock, info)
+#define cc_to_vclock(d) container_of((d), struct ptp_vclock, cc)
+#define dw_to_vclock(d) container_of((d), struct ptp_vclock, refresh_work)
+
+struct ptp_vclock {
+	struct ptp_clock *pclock;
+	struct ptp_clock_info info;
+	struct ptp_clock *clock;
+
+	/* timecounter/cyclecounter definitions */
+	struct cyclecounter cc;
+	struct timecounter tc;
+	spinlock_t lock;	/* protects tc/cc */
+	struct delayed_work refresh_work;
+	unsigned long refresh_interval;
+	u32 mult;
+	u32 mult_factor;
+	u32 div_factor;
+};
+
 /*
  * The function queue_cnt() is safe for readers to call without
  * holding q->lock. Readers use this function to verify that the queue
@@ -89,4 +110,6 @@ extern const struct attribute_group *ptp_groups[];
 int ptp_populate_pin_groups(struct ptp_clock *ptp);
 void ptp_cleanup_pin_groups(struct ptp_clock *ptp);
 
+struct ptp_vclock *ptp_vclock_register(struct ptp_clock *pclock);
+void ptp_vclock_unregister(struct ptp_vclock *vclock);
 #endif
