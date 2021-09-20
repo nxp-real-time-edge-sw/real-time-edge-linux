a4f1b5e9ef980551e4dfc103441112175b658280
Marouen Ghodhbane <marouen.ghodhbane@nxp.com>
2021-09-20 16:58:47 +0200

    net: fec: avb: check if fec_enet_avb_get_device() call is related to fec device or not
    - Some SoCs (like i.mx8mp) have multiple network interfaces attached to different controllers: eqos and fec
    Here make sure to return only device structure related to fec driver.
    
    Signed-off-by: Marouen Ghodhbane <marouen.ghodhbane@nxp.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -5021,15 +5021,32 @@ static const unsigned short offset_des_active_txq[] = {
 };
 
 #ifdef CONFIG_AVB_SUPPORT
+
+static struct platform_driver fec_driver;
+
+/* Checks if the net_device is registered by the fec */
+static bool __is_fec_net_device(struct net_device *ndev)
+{
+	if (!ndev)
+		return false;
+
+	if (ndev->dev.parent->driver == &fec_driver.driver)
+		return true;
+	else
+		return false;
+}
+
 struct device *fec_enet_avb_get_device(const char *ifname)
 {
 	struct net_device *ndev;
 	struct fec_enet_private *fep;
 
-
 	ndev = dev_get_by_name(&init_net, ifname);
 	if (!ndev)
-		return NULL;
+		goto err_dev_get;
+
+	if (!__is_fec_net_device(ndev))
+		goto err_ndev;
 
 	fep = netdev_priv(ndev);
 
@@ -5037,6 +5054,11 @@ struct device *fec_enet_avb_get_device(const char *ifname)
 
 	return &fep->pdev->dev;
 
+err_ndev:
+	dev_put(ndev);
+
+err_dev_get:
+	return NULL;
 }
 EXPORT_SYMBOL(fec_enet_avb_get_device);
 
@@ -5050,6 +5072,9 @@ int fec_enet_avb_register(const char *ifname, const struct avb_ops *avb, void *d
 	ndev = dev_get_by_name(&init_net, ifname);
 	if (!ndev)
 		goto err_dev_get;
+
+	if (!__is_fec_net_device(ndev))
+		goto err_ndev;
 
 	fep = netdev_priv(ndev);
 
@@ -5078,6 +5103,7 @@ int fec_enet_avb_register(const char *ifname, const struct avb_ops *avb, void *d
 	return ifindex;
 
 err_avb:
+err_ndev:
 	dev_put(ndev);
 
 err_dev_get:
@@ -5094,6 +5120,9 @@ int fec_enet_avb_unregister(int ifindex, const struct avb_ops *avb)
 	ndev = dev_get_by_index(&init_net, ifindex);
 	if (!ndev)
 		goto err_dev_get;
+
+	if (!__is_fec_net_device(ndev))
+		goto err_ndev;
 
 	fep = netdev_priv(ndev);
 	if (fep->avb != avb)
@@ -5119,6 +5148,7 @@ int fec_enet_avb_unregister(int ifindex, const struct avb_ops *avb)
 	return 0;
 
 err_avb:
+err_ndev:
 	dev_put(ndev);
 
 err_dev_get:
@@ -5134,6 +5164,9 @@ int fec_enet_get_tx_queue_properties(int ifindex, struct tx_queue_properties *pr
 	ndev = dev_get_by_index(&init_net, ifindex);
 	if (!ndev)
 		goto err_dev_get;
+
+	if (!__is_fec_net_device(ndev))
+		goto err_ndev;
 
 	fep = netdev_priv(ndev);
 
@@ -5168,6 +5201,7 @@ int fec_enet_get_tx_queue_properties(int ifindex, struct tx_queue_properties *pr
 	return 0;
 
 err_queues:
+err_ndev:
 	dev_put(ndev);
 
 err_dev_get:
