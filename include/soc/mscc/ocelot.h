da5788e9843dba983ec0341e3a8483a7c5058fee
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-12-28 18:26:48 +0800

    net: mscc: ocelot: allow a config where all bridge VLANs are egress-untagged
    At present, the ocelot driver accepts a single egress-untagged bridge
    VLAN, meaning that this sequence of operations:
    
    ip link add br0 type bridge vlan_filtering 1
    ip link set swp0 master br0
    bridge vlan add dev swp0 vid 2 pvid untagged
    
    fails because the bridge automatically installs VID 1 as a pvid & untagged
    VLAN, and vid 2 would be the second untagged VLAN on this port. It is
    necessary to delete VID 1 before proceeding to add VID 2.
    
    This limitation comes from the fact that we operate the port tag, when
    it has an egress-untagged VID, in the OCELOT_PORT_TAG_NATIVE mode.
    The ocelot switches do not have full flexibility and can either have one
    single VID as egress-untagged, or all of them.
    
    There are use cases for having all VLANs as egress-untagged as well, and
    this patch adds support for that.
    
    The change rewrites ocelot_port_set_native_vlan() into a more generic
    ocelot_port_manage_port_tag() function. Because the software bridge's
    state, transmitted to us via switchdev, can become very complex, we
    don't attempt to track all possible state transitions, but instead take
    a more declarative approach and just make ocelot_port_manage_port_tag()
    figure out which more to operate in:
    
    - port is VLAN-unaware: the classified VLAN (internal, unrelated to the
                            802.1Q header) is not inserted into packets on egress
    - port is VLAN-aware:
      - port has tagged VLANs:
        -> port has no untagged VLAN: set up as pure trunk
        -> port has one untagged VLAN: set up as trunk port + native VLAN
        -> port has more than one untagged VLAN: this is an invalid config
           which is rejected by ocelot_vlan_prepare
      - port has no tagged VLANs
        -> set up as pure egress-untagged port
    
    We don't keep the number of tagged and untagged VLANs, we just count the
    structures we keep.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 0da1a1c4891188f456c7790940e47c8043bc7c9b)
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    (conflicts in drivers/net/ethernet/mscc/ocelot.c because we did not
     backport the extack support and the removal of switchdev transactional
     model)
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

--- include/soc/mscc/ocelot.h
+++ include/soc/mscc/ocelot.h
@@ -582,6 +582,7 @@ struct ocelot_vlan {
 struct ocelot_bridge_vlan {
 	u16 vid;
 	unsigned long portmask;
+	unsigned long untagged;
 	struct list_head list;
 };
 
@@ -618,8 +619,6 @@ struct ocelot_port {
 
 	/* VLAN that untagged frames are classified to, on ingress */
 	struct ocelot_vlan		pvid_vlan;
-	/* The VLAN ID that will be transmitted as untagged, on egress */
-	struct ocelot_vlan		native_vlan;
 
 	unsigned int			ptp_skbs_in_flight;
 	u8				ptp_cmd;
