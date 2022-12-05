75c30bd84fd453bd0b70be1ee6764fdc3ec98c0d
Wenbin Song <wenbin.song@nxp.com>
2022-12-05 20:25:06 +0800

    fec_ecat: add fec native driver for raw packet proto
    the fec native driver will bypass Linux TCP/IP stack to transmit
    and receive packets directly to/from the hardware queues.
    
    Signed-off-by: Wenbin Song <wenbin.song@nxp.com>
    Signed-off-by: qizhang <jony.zhang@nxp.com>
    resolved conflicts:
    	net/socket.c
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- net/socket.c
+++ net/socket.c
@@ -110,6 +110,10 @@
 #include <linux/ptp_clock_kernel.h>
 #include <trace/events/sock.h>
 
+static int fast_raw_socket_fd = -1;
+static struct net_device *fast_raw_socket_dev;
+static struct socket *fast_raw_socket_sock = NULL;
+
 #ifdef CONFIG_NET_RX_BUSY_POLL
 unsigned int sysctl_net_busy_read __read_mostly;
 unsigned int sysctl_net_busy_poll __read_mostly;
@@ -670,6 +674,10 @@ static void __sock_release(struct socket *sock, struct inode *inode)
 	if (!sock->file) {
 		iput(SOCK_INODE(sock));
 		return;
+	}
+	if (fast_raw_socket_sock != NULL && fast_raw_socket_sock == sock) {
+		fast_raw_socket_sock = NULL;
+		fast_raw_socket_fd = -1;
 	}
 	sock->file = NULL;
 }
@@ -1853,6 +1861,15 @@ int __sys_bind(int fd, struct sockaddr __user *umyaddr, int addrlen)
 						      &address, addrlen);
 		}
 		fput_light(sock->file, fput_needed);
+		if (fast_raw_socket_fd < 0) {
+			if (sock->type == SOCK_RAW && sock->sk->sk_family == PF_PACKET) {
+				if (sock->ndev != NULL && sock->ndev->fast_raw_device == 1) {
+					fast_raw_socket_fd = fd;
+					fast_raw_socket_dev = sock->ndev;
+					fast_raw_socket_sock = sock;
+				}
+			}
+		}
 	}
 	return err;
 }
@@ -2169,6 +2186,10 @@ int __sys_sendto(int fd, void __user *buff, size_t len, unsigned int flags,
 	struct msghdr msg;
 	struct iovec iov;
 	int fput_needed;
+	if (fd == fast_raw_socket_fd) {
+		err = fast_raw_socket_dev->netdev_ops->ndo_fast_xmit(fast_raw_socket_dev, buff, len);
+		return err;
+	}
 
 	err = import_single_range(ITER_SOURCE, buff, len, &iov, &msg.msg_iter);
 	if (unlikely(err))
@@ -2190,6 +2211,7 @@ int __sys_sendto(int fd, void __user *buff, size_t len, unsigned int flags,
 		msg.msg_namelen = addr_len;
 	}
 	flags &= ~MSG_INTERNAL_SENDMSG_FLAGS;
+
 	if (sock->file->f_flags & O_NONBLOCK)
 		flags |= MSG_DONTWAIT;
 	msg.msg_flags = flags;
@@ -2235,6 +2257,11 @@ int __sys_recvfrom(int fd, void __user *ubuf, size_t size, unsigned int flags,
 	struct iovec iov;
 	int err, err2;
 	int fput_needed;
+	int i;
+	if (fd == fast_raw_socket_fd) {
+		err = fast_raw_socket_dev->netdev_ops->ndo_fast_recv(fast_raw_socket_dev, ubuf, size, addr, addr_len);
+		return err;
+	}
 
 	err = import_single_range(ITER_DEST, ubuf, size, &iov, &msg.msg_iter);
 	if (unlikely(err))
