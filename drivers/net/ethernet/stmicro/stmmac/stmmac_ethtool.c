14183677bd499646329fb20ffc1410515eaaaa72
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2023-05-23 11:38:25 +0800

    net: stmmac: update preemption lldp verify logic
    The lldpd tries to enable preemption when it is determined that the link
    partner supports preemption, and disable preemption when it is determined
    that the link partner doesn't support preemption.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Signed-off-by: Tao Yang <Tao.Yang1@nxp.com>
    Resolved conflicts:
    	drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
    Adapted the updated stmmac_fpe_configure():
    	drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
    	drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
    TODO: double-check
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
@@ -1265,12 +1265,22 @@ static int stmmac_set_tunable(struct net_device *dev,
 static int stmmac_reset_preempt(struct net_device *dev, bool enable)
 {
 	struct stmmac_priv *priv = netdev_priv(dev);
+	struct stmmac_fpe_cfg *fpe_cfg = priv->plat->fpe_cfg;
+	bool *hs_enable = &fpe_cfg->hs_enable;
 
-	stmmac_fpe_configure(priv, priv->ioaddr, priv->plat->fpe_cfg,
-			     priv->plat->tx_queues_to_use,
-			     priv->plat->rx_queues_to_use, enable, NULL);
+	priv->plat->fpe_cfg->enable = enable;
 
-	stmmac_fpe_handshake(priv, priv->fp_verify);
+	if (enable) {
+		if (!*hs_enable) {
+			stmmac_fpe_configure(priv, priv->ioaddr, fpe_cfg,
+					     priv->plat->tx_queues_to_use,
+					     priv->plat->rx_queues_to_use, true, NULL);
+		}
+	} else {
+		stmmac_fpe_configure(priv, priv->ioaddr, fpe_cfg,
+				     priv->plat->tx_queues_to_use,
+				     priv->plat->rx_queues_to_use, false, NULL);
+	}
 
 	return 0;
 }
@@ -1294,12 +1304,6 @@ static int stmmac_set_preempt(struct net_device *dev, struct ethtool_fp *fpcmd)
 		fpe.enable = 1;
 	else
 		fpe.enable = fpcmd->fp_enabled ? 0 : 1;
-
-	if (fpcmd->fp_lldp_verify)
-		fpe.enable = 0;
-
-	priv->fp_lldp_verify = fpcmd->fp_lldp_verify;
-	priv->fp_verify = fpcmd->fp_enabled;
 
 	/* To support preemption MAC should have more than 1 TX queue with at
 	 * least 1 Queue designated as Express Queue. Queue 0 is always used as
@@ -1344,14 +1348,13 @@ static int stmmac_get_preempt(struct net_device *dev, struct ethtool_fp *fpcmd)
 	}
 
 	fpcmd->fp_supported = 1;
-	fpcmd->fp_status = priv->fp_lldp_verify ? 1 :
-			   (priv->fp_verify ? 1 : fpe.enable);
+	fpcmd->fp_status = priv->plat->fpe_cfg->enable;
 	fpcmd->fp_active = fpe.enable;
 	fpcmd->supported_queues_mask = GENMASK(priv->plat->tx_queues_to_use - 1,
 					       0);
 	fpcmd->preemptible_queues_mask = fpe.p_queues;
-	/* Queue 0 is always preemption when preemption is enabled. */
-	if (fpcmd->fp_status)
+	/* Queue 0 is always preemption when preemption is active. */
+	if (fpcmd->fp_active)
 		fpcmd->preemptible_queues_mask |= 1;
 	fpcmd->min_frag_size = (fpe.fragsize + 1) * 64 - 4;
 
--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -969,12 +969,12 @@ static void stmmac_fpe_link_state_handle(struct stmmac_priv *priv, bool is_up)
 	} else {
 		*lo_state = FPE_STATE_OFF;
 		*lp_state = FPE_STATE_OFF;
-		if (*hs_enable)
-			stmmac_fpe_configure(priv, priv->ioaddr,
-					     fpe_cfg,
-					     priv->plat->tx_queues_to_use,
-					     priv->plat->rx_queues_to_use,
-					     0, NULL);
+		priv->plat->fpe_cfg->enable = false;
+		stmmac_fpe_configure(priv, priv->ioaddr,
+				     fpe_cfg,
+				     priv->plat->tx_queues_to_use,
+				     priv->plat->rx_queues_to_use,
+				     false, NULL);
 	}
 }
 
@@ -7268,7 +7268,6 @@ static void stmmac_fpe_lp_task(struct work_struct *work)
 	enum stmmac_fpe_state *lo_state = &fpe_cfg->lo_fpe_state;
 	enum stmmac_fpe_state *lp_state = &fpe_cfg->lp_fpe_state;
 	bool *hs_enable = &fpe_cfg->hs_enable;
-	bool enable = fpe_cfg->enable;
 	int retries = 20;
 
 	while (retries-- > 0) {
@@ -7282,7 +7281,7 @@ static void stmmac_fpe_lp_task(struct work_struct *work)
 					     fpe_cfg,
 					     priv->plat->tx_queues_to_use,
 					     priv->plat->rx_queues_to_use,
-					     enable, NULL);
+					     true, NULL);
 
 			netdev_info(priv->dev, "configured FPE\n");
 
