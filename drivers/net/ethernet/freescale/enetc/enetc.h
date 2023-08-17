5ff203abeac8e49adcaf6d345b55ffbcffcdcf2e
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2023-08-17 15:41:36 +0800

    net: enetc: tsn: reset gcl of qbv when ptp clock adjust
    The basetime of Qbv is not corrected when ptp clock adjust, this will
    cause GCL to hang waiting for the new ptp time to arrive at the next
    period time. Reset Qbv's GCL allows GCL to work directly based on new
    ptp clock time.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Fix build warning:
    drivers/net/ethernet/freescale/enetc/enetc_ptp.c:13:5: warning: no previous prototype for ‘ptp_enetc_settime’ [-Wmissing-prototypes]
    RTEv3.4: resolve conflicts:
    	drivers/net/ethernet/freescale/enetc/enetc_ptp.c
    	drivers/net/ethernet/freescale/enetc/enetc_tsn.c
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- drivers/net/ethernet/freescale/enetc/enetc.h
+++ drivers/net/ethernet/freescale/enetc/enetc.h
@@ -801,6 +801,7 @@ static inline int enetc_set_psfp(struct net_device *ndev, bool en)
 
 void enetc_tsn_pf_init(struct net_device *netdev, struct pci_dev *pdev);
 void enetc_tsn_pf_deinit(struct net_device *netdev);
+void enetc_ptp_clock_update(void);
 
 #else
 
@@ -812,4 +813,7 @@ static inline void enetc_tsn_pf_deinit(struct net_device *netdev)
 {
 }
 
+static inline void enetc_ptp_clock_update(void)
+{
+}
 #endif
