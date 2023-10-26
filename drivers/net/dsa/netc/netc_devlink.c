3829f293d01e374c5b1e6f9a13537d9ba52bec9d
Minghuan Lian <Minghuan.Lian@nxp.com>
2023-10-26 11:35:40 +0800

    net: dsa: netc: add driver for NETC switch
    This supports NETC switch an TSN Ethernet Switch from i.MX RT1180.
    one i.MX RT1180 switch port or other ethernet port is connected to
    MPU as data path. And, i.MX RT1180 as a SPI deive from MPU provides
    contro path.
    
    The driver uses NETC tag driver to rx/tx packets, and SPI transfer
    to rx/tx the control command.
    
    Signed-off-by: Minghuan Lian <Minghuan.Lian@nxp.com>

---
+++ drivers/net/dsa/netc/netc_devlink.c
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright 2023 NXP
+ */
+
+#include "netc.h"
+
+static size_t netc_config_get_size(struct netc_private *priv)
+{
+	return sizeof(struct netc_config);
+}
+
+static int
+netc_region_config_snapshot(struct devlink *dl,
+				   const struct devlink_region_ops *ops,
+				   struct netlink_ext_ack *extack,
+				   u8 **data)
+{
+	struct dsa_switch *ds = dsa_devlink_to_ds(dl);
+	struct netc_private *priv = ds->priv;
+	size_t len;
+
+	len = netc_config_get_size(priv);
+	*data = kcalloc(len, sizeof(u8), GFP_KERNEL);
+	if (!*data)
+		return -ENOMEM;
+
+	return netc_xfer_get_cmd(priv, NETC_CMD_SYS_INFO_GET, 0, *data, len);
+}
+
+static struct devlink_region_ops netc_region_config_ops = {
+	.name = "config",
+	.snapshot = netc_region_config_snapshot,
+	.destructor = kfree,
+};
+
+enum netc_region_id {
+	NETC_REGION_CONFIG = 0,
+};
+
+struct netc_region {
+	const struct devlink_region_ops *ops;
+	size_t (*get_size)(struct netc_private *priv);
+};
+
+static struct netc_region netc_regions[] = {
+	[NETC_REGION_CONFIG] = {
+		.ops = &netc_region_config_ops,
+		.get_size = netc_config_get_size,
+	},
+};
+
+int netc_devlink_info_get(struct dsa_switch *ds,
+			  struct devlink_info_req *req,
+			  struct netlink_ext_ack *extack)
+{
+	struct netc_private *priv = ds->priv;
+	int rc;
+
+	rc = devlink_info_driver_name_put(req, "netc");
+	if (rc)
+		return rc;
+
+	rc = devlink_info_version_fixed_put(req,
+				DEVLINK_INFO_VERSION_GENERIC_ASIC_ID,
+				priv->info->name);
+	return rc;
+}
+
+int netc_devlink_setup(struct dsa_switch *ds)
+{
+	int i, num_regions = ARRAY_SIZE(netc_regions);
+	struct netc_private *priv = ds->priv;
+	const struct devlink_region_ops *ops;
+	struct devlink_region *region;
+	u64 size;
+
+	priv->regions = kcalloc(num_regions, sizeof(struct devlink_region *),
+				GFP_KERNEL);
+	if (!priv->regions)
+		return -ENOMEM;
+
+	for (i = 0; i < num_regions; i++) {
+		size = netc_regions[i].get_size(priv);
+		ops = netc_regions[i].ops;
+
+		region = dsa_devlink_region_create(ds, ops, 1, size);
+		if (IS_ERR(region)) {
+			while (--i >= 0)
+				dsa_devlink_region_destroy(priv->regions[i]);
+
+			kfree(priv->regions);
+			return PTR_ERR(region);
+		}
+
+		priv->regions[i] = region;
+	}
+
+	return 0;
+}
+
+void netc_devlink_teardown(struct dsa_switch *ds)
+{
+	int i, num_regions = ARRAY_SIZE(netc_regions);
+	struct netc_private *priv = ds->priv;
+
+	for (i = 0; i < num_regions; i++)
+		dsa_devlink_region_destroy(priv->regions[i]);
+
+	kfree(priv->regions);
+}
---
+++ drivers/net/dsa/netc/netc_spi.c
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright 2023 NXP
+ */
+
+#include <linux/spi/spi.h>
+#include "netc.h"
+
+int netc_xfer_cmd(const struct netc_private *priv,
+		  enum netc_spi_rw_mode rw, enum netc_cmd cmd,
+		  void *param, size_t param_len,
+		  void *resp, size_t resp_len)
+{
+	struct netc_cmd_hdr hdr = {0};
+	struct spi_device *spi = priv->spidev;
+	struct spi_transfer hdr_xfer, resp_xfer;
+	int rc;
+
+	if (!IS_ALIGNED(resp_len, NETC_SPI_MSG_WORD_BYTES)) {
+		dev_err(&spi->dev, "netc cmd %d data size should be a multiple of 4  : %ld",
+			 cmd, resp_len);
+		return -EINVAL;
+	}
+
+	if (resp_len > priv->max_xfer_len) {
+		dev_err(&spi->dev, "netc cmd %d data size is too large\n",
+			cmd);
+		return -EINVAL;
+	}
+
+	if (param_len > NETC_SPI_MSG_PARAM_SIZE) {
+		dev_err(&spi->dev, "netc cmd %d param size is too large\n",
+			cmd);
+		return -EINVAL;
+	}
+
+	hdr.cmd = (rw << NETC_CMD_DIR_SHIFT) |
+		  ((resp_len / NETC_SPI_MSG_WORD_BYTES) <<
+		   NETC_CMD_LEN_SHIFT) |
+		  cmd;
+	if (param)
+		memcpy(hdr.param, param, param_len);
+
+	hdr_xfer.tx_buf = &hdr;
+	hdr_xfer.len = NETC_SPI_MSG_HEADER_SIZE;
+
+	rc = spi_sync_transfer(spi, &hdr_xfer, 1);
+	if (rc < 0) {
+		dev_err(&spi->dev, "netc cmd %d SPI transfer failed: %d\n",
+			cmd, rc);
+		return rc;
+	}
+
+	usleep_range(NETC_SPI_MSG_RESPONSE_TIME,
+		     NETC_SPI_MSG_RESPONSE_TIME * 10);
+
+	if (!resp)
+		return 0;
+
+	/* Populate the transfer's data buffer */
+	if (rw == SPI_READ)
+		resp_xfer.rx_buf = resp;
+	else
+		resp_xfer.tx_buf = resp;
+	resp_xfer.len = resp_len;
+
+	rc = spi_sync_transfer(spi, &resp_xfer, 1);
+	if (rc < 0) {
+		dev_err(&spi->dev, "netc cmd %d SPI transfer failed: %d\n",
+			cmd, rc);
+		return rc;
+	}
+
+	usleep_range(NETC_SPI_MSG_RESPONSE_TIME,
+		     NETC_SPI_MSG_RESPONSE_TIME * 10);
+
+	return 0;
+}
+
+int netc_xfer_set_cmd(const struct netc_private *priv,
+		      enum netc_cmd cmd,
+		      void *param, size_t param_len)
+{
+	return netc_xfer_cmd(priv, SPI_WRITE, cmd,
+			     param, param_len,
+			     NULL, 0);
+}
+
+int netc_xfer_get_cmd(const struct netc_private *priv,
+			enum netc_cmd cmd, uint32_t id,
+			void *resp, size_t resp_len)
+{
+	struct netc_cmd_read_param param;
+
+	param.id = id;
+
+	return netc_xfer_cmd(priv, SPI_READ, cmd,
+			     &param, sizeof(param),
+			     resp, resp_len);
+}
+
+int netc_xfer_write_reg(const struct netc_private *priv,
+			uint32_t reg, uint32_t value)
+{
+	struct netc_cmd_reg_cmd reg_cmd;
+
+	reg_cmd.reg = reg;
+	reg_cmd.value = value;
+
+	return netc_xfer_set_cmd(priv, NETC_CMD_REG_SET,
+				 &reg_cmd, sizeof(reg_cmd));
+}
+
+int netc_xfer_read_reg(const struct netc_private *priv,
+		       uint32_t reg, uint32_t *value)
+{
+	return netc_xfer_get_cmd(priv, NETC_CMD_REG_GET, reg,
+				 value, sizeof(*value));
+}
