e1d9205d9b0ab05ae2e1696d22978954cd14b8d5
Marouen Ghodhbane <marouen.ghodhbane@nxp.com>
2024-09-24 09:32:03 +0200

    net: stmmac: add queue index to napi instance names
    Adding the queue index is usefule to better tune the system for low latency (higher
    prio) on specific network queue (e.g for TSN traffic) processing.
    
    Signed-off-by: Marouen Ghodhbane <marouen.ghodhbane@nxp.com>

--- drivers/net/ethernet/freescale/enetc/enetc.c
+++ drivers/net/ethernet/freescale/enetc/enetc.c
@@ -3718,6 +3718,7 @@ static int enetc_bdr_init(struct enetc_ndev_priv *priv, int i, int v_tx_rings)
 {
 	struct enetc_int_vector *v __free(kfree);
 	struct enetc_bdr *bdr;
+	char name[NAPINAMSIZ];
 	int j, err;
 
 	v = kzalloc(struct_size(v, tx_ring, v_tx_rings), GFP_KERNEL);
@@ -3751,7 +3752,10 @@ static int enetc_bdr_init(struct enetc_ndev_priv *priv, int i, int v_tx_rings)
 		v->rx_dim_en = true;
 	}
 	INIT_WORK(&v->rx_dim.work, enetc_rx_dim_work);
-	netif_napi_add(priv->ndev, &v->napi, enetc_poll);
+
+	snprintf(name, NAPINAMSIZ, "rxtx-%d", i);
+	netif_napi_add_named(priv->ndev, &v->napi, enetc_poll,
+		NAPI_POLL_WEIGHT, name);
 	v->count_tx_rings = v_tx_rings;
 
 	for (j = 0; j < v_tx_rings; j++) {
