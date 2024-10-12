72bc664feb34d924c623ee7d7f0e0372befc8276
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2024-10-12 14:44:53 +0800

    net: felix: fix gcl configuration lost when clock adjust
    The Gate control list configuration will be lost when the PTP clock
    adjust. Re-configured the GCL when PTP clock adjust.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

--- drivers/net/dsa/ocelot/felix_vsc9959.c
+++ drivers/net/dsa/ocelot/felix_vsc9959.c
@@ -1556,14 +1556,14 @@ static void vsc9959_tas_clock_adjust(struct ocelot *ocelot)
 		if (!taprio)
 			continue;
 
+		/* Disable time-aware shaper */
+		ocelot_rmw_rix(ocelot, 0, QSYS_TAG_CONFIG_ENABLE,
+			       QSYS_TAG_CONFIG, port);
+
 		ocelot_rmw(ocelot,
 			   QSYS_TAS_PARAM_CFG_CTRL_PORT_NUM(port),
 			   QSYS_TAS_PARAM_CFG_CTRL_PORT_NUM_M,
 			   QSYS_TAS_PARAM_CFG_CTRL);
-
-		/* Disable time-aware shaper */
-		ocelot_rmw_rix(ocelot, 0, QSYS_TAG_CONFIG_ENABLE,
-			       QSYS_TAG_CONFIG, port);
 
 		vsc9959_new_base_time(ocelot, taprio->base_time,
 				      taprio->cycle_time, &base_ts);
@@ -1576,6 +1576,9 @@ static void vsc9959_tas_clock_adjust(struct ocelot *ocelot)
 			   QSYS_PARAM_CFG_REG_3_BASE_TIME_SEC_MSB(val),
 			   QSYS_PARAM_CFG_REG_3_BASE_TIME_SEC_MSB_M,
 			   QSYS_PARAM_CFG_REG_3);
+
+		for (int i = 0; i < taprio->num_entries; i++)
+			vsc9959_tas_gcl_set(ocelot, i, &taprio->entries[i]);
 
 		ocelot_rmw(ocelot, QSYS_TAS_PARAM_CFG_CTRL_CONFIG_CHANGE,
 			   QSYS_TAS_PARAM_CFG_CTRL_CONFIG_CHANGE,
