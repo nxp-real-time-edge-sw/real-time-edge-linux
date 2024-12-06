cbbf776c9a98e734fd7c32408fe979faf681a6bb
Wei Fang <wei.fang@nxp.com>
2024-12-06 14:13:04 +0800

    LF-14857-2 net: enetc: don't interrupt Rx reception due to bad packet
    Previously, the Rx reception would be interrupted when a bad packet was
    received. If there were still packets on the Rx BD ring, they could only
    be received at the next interrupt, which could degrade performance and
    increase latency. So it is better to continue receiving packets on the
    Rx BD ring after processing the bad packet.
    
    Signed-off-by: Wei Fang <wei.fang@nxp.com>

--- drivers/net/ethernet/freescale/enetc/enetc.c
+++ drivers/net/ethernet/freescale/enetc/enetc.c
@@ -1509,13 +1509,15 @@ static void enetc_add_rx_buff_to_skb(struct enetc_bdr *rx_ring, int i,
 
 static bool enetc_check_bd_errors_and_consume(struct enetc_bdr *rx_ring,
 					      u32 bd_status,
-					      union enetc_rx_bd **rxbd, int *i)
+					      union enetc_rx_bd **rxbd, int *i,
+					      int *cleaned_cnt)
 {
 	if (likely(!(bd_status & ENETC_RXBD_LSTATUS(ENETC_RXBD_ERR_MASK))))
 		return false;
 
 	enetc_put_rx_buff(rx_ring, &rx_ring->rx_swbd[*i]);
 	enetc_rxbd_next(rx_ring, rxbd, i);
+	(*cleaned_cnt)++;
 
 	while (!(bd_status & ENETC_RXBD_LSTATUS_F)) {
 		dma_rmb();
@@ -1523,6 +1525,7 @@ static bool enetc_check_bd_errors_and_consume(struct enetc_bdr *rx_ring,
 
 		enetc_put_rx_buff(rx_ring, &rx_ring->rx_swbd[*i]);
 		enetc_rxbd_next(rx_ring, rxbd, i);
+		(*cleaned_cnt)++;
 	}
 
 	rx_ring->ndev->stats.rx_dropped++;
@@ -1613,8 +1616,8 @@ static int enetc_clean_rx_ring(struct enetc_bdr *rx_ring,
 		dma_rmb(); /* for reading other rxbd fields */
 
 		if (enetc_check_bd_errors_and_consume(rx_ring, bd_status,
-						      &rxbd, &i))
-			break;
+						      &rxbd, &i, &cleaned_cnt))
+			continue;
 
 		skb = enetc_build_skb(rx_ring, bd_status, &rxbd, &i,
 				      &cleaned_cnt, ENETC_RXB_DMA_SIZE);
@@ -1986,8 +1989,8 @@ static int enetc_clean_rx_ring_xdp(struct enetc_bdr *rx_ring,
 		dma_rmb(); /* for reading other rxbd fields */
 
 		if (enetc_check_bd_errors_and_consume(rx_ring, bd_status,
-						      &rxbd, &i))
-			break;
+						      &rxbd, &i, &cleaned_cnt))
+			continue;
 
 		orig_rxbd = rxbd;
 		orig_cleaned_cnt = cleaned_cnt;
