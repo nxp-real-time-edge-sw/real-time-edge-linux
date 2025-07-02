4315cd008ea4b643b46d64cba72b56c1d5724bee
Laurent Brando <laurent.brando@nxp.com>
2025-07-02 19:00:12 +0200

    ptp: ptp_netc: add support for converttime callback
    NETC hardware provides support for a free-running counter
    allowing to keep hardware clock adjustable while
    virtual clocks are enabled.
    
    Adding support for the converttime callback to perform
    conversion between cycles and hardware clocks.
    
    Signed-off-by: Laurent Brando <laurent.brando@nxp.com>
    RTEv3.4: resolve conflicts:
    	drivers/ptp/ptp_netc.c
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- drivers/ptp/ptp_netc.c
+++ drivers/ptp/ptp_netc.c
@@ -865,7 +865,7 @@ int netc_timer_ptp_convert(struct pci_dev *timer_pdev, u64 ts_src, u64 *ts_dst, 
 	}
 
 	if (cycles) {
-		/* convert from synchronized to free-running */
+		/* convert from hardware to free-running cycles */
 		dt = ns_src - srt_ns;
 
 		if (dt < 0)
@@ -875,7 +875,7 @@ int netc_timer_ptp_convert(struct pci_dev *timer_pdev, u64 ts_src, u64 *ts_dst, 
 
 		ns_dst = frt_ns + dt_scaled;
 	} else {
-		/* convert from free-running to synchronized */
+		/* convert from free-running cycles to hardware */
 		dt = ns_src - frt_ns;
 
 		if (dt < 0)
@@ -911,6 +911,21 @@ static int netc_timer_settime64(struct ptp_clock_info *ptp,
 	return 0;
 }
 
+static int netc_timer_converttime(struct ptp_clock_info *ptp, struct timespec64 src_ts,
+				  struct timespec64 *dst_ts, bool cycles)
+{
+	struct netc_timer *priv = ptp_to_netc_timer(ptp);
+	u64 dst_ns;
+	int rc;
+
+	rc = netc_timer_ptp_convert(priv->pdev, timespec64_to_ns(&src_ts), &dst_ns,
+				    !cycles, cycles);
+	if (!rc)
+		*dst_ts = ns_to_timespec64(dst_ns);
+
+	return rc;
+}
+
 static const struct ptp_clock_info netc_timer_ptp_caps = {
 	.owner		= THIS_MODULE,
 	.name		= "NETC Timer PTP clock",
@@ -931,6 +946,7 @@ static const struct ptp_clock_info netc_timer_ptp_caps = {
 	.settime64	= netc_timer_settime64,
 	.enable		= netc_timer_enable,
 	.perout_loopback = netc_timer_perout_loopback,
+	.converttime	= netc_timer_converttime,
 };
 
 static void netc_timer_init(struct netc_timer *priv)
